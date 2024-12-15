3e605580075d5db144c30f8b218b14bb39520008
AdrakPro <adrak99.adrak99@gmail.com>
2024-12-16 00:40:02 +0100

    Added appointment router to main router

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -5,16 +5,19 @@ import doctorRouter from "@doctor/router";
 import patientRouter from "@patient/router";
 import auditLogRouter from "@auditLog/router";
 import departmentRouter from "@department/router";
+import appointmentRouter from "@appointment/router";
 
 const mainRouter = express.Router();
 
-mainRouter.use("/api",
+mainRouter.use(
+  "/api",
   personRouter,
   prescriptionRouter,
   doctorRouter,
   patientRouter,
   auditLogRouter,
   departmentRouter,
+  appointmentRouter,
 );
 
 export default mainRouter;
