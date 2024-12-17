feeb99cbe4535f2ad86a63c7a6d16c5ef63eb784
AdrakPro <adrak99.adrak99@gmail.com>
2024-12-17 17:38:50 +0100

    Added self-assigned HTTPS server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { errorMiddleware } from "@common/middlewares/errorMiddleware";
 import mainRouter from "@/router";
+import authRouter from "@common/auth/router";
+import * as fs from "node:fs";
+import * as https from "node:https";
+import path from "path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cookieParser());
 app.use(express.json({ limit: "5kb" }));
 
+app.use(authRouter);
 app.use(mainRouter);
 
 app.use(errorMiddleware);
@@ -18,8 +29,12 @@ app.use((req, res, next) => {
   });
 });
 
+const privateKey = fs.readFileSync(path.resolve(__dirname, "https/server.key"), "utf8");
+const certificate = fs.readFileSync(path.resolve(__dirname, "https/server.cert"), "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+https.createServer(credentials, app).listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
