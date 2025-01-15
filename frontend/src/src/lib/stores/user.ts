dfba0cf8dbfecd663d9134d315719aecc982d1f0
Piotr Rafałko <164638790+RflPtrk@users.noreply.github.com>
2025-01-15 19:57:29 +0100

    Delete frontend/src/src directory (wrong placement)

--- frontend/src/src/lib/stores/user.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-type User = {
-    personId: string;
-    role: 'ADMIN' | 'DOCTOR' | 'PATIENT';
-    token: string;
-};
-
-export const user = writable<User | null>(null); // Store can be null if no user is logged in
